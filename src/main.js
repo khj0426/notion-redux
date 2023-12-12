9c54980d23cfb0499b15caf84d4979c5d4a45a1c
Kim0426 <706shin1728@naver.com>
2023-12-13 01:46:50 +0900

    feat: webpack 적용 후 스타일 적용 안되는 문제 수정

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { default as App } from './App.js'
-const $app = document.querySelector('#app')
-
+import { default as App } from './App.js';
+import './editor.css';
+import './index.css';
+import './document.css';
+const $app = document.querySelector('#app');
 // main.js가 시작점으로 잡고 App을 만든다.
 const app = new App({
   $target: $app,
-  initalState: []
-})
+  initalState: [],
+});
